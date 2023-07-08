e1d036262e3a420b73c959913ce5dd57bfd1b9f6
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-07-08 14:27:44 -0700

    Organize files

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-/***************************************************************************/
 const body      = document.querySelector('body');
-const mainNav   = document.querySelector('.navigation');
-const navToggle = document.querySelector('.mobile-menu-icons');
+const mainNav   = document.querySelector('.c-navigation');
+const navToggle = document.querySelector('.f-mobileNavIcons');
 
-window.addEventListener("resize", () => {
-    if (window.innerWidth >= 768) {
-        mainNav  .setAttribute("data-visible",  false);
-        navToggle.setAttribute("aria-expanded", false);
-        body.style.overflowY = "scroll";
-    }
-});
 navToggle.addEventListener('click', () => {
     const vis = mainNav.getAttribute("data-visible");
     if (vis === "false") {
@@ -20,48 +12,6 @@ navToggle.addEventListener('click', () => {
     } else if (vis === "true") {
         mainNav  .setAttribute("data-visible",  false);
         navToggle.setAttribute("aria-expanded", false);
-        body.style.overflowY = "scroll";
+        body.style.overflowY = "scroll"
     }
 })
-mainNav.addEventListener("click", () => {
-    mainNav  .setAttribute("data-visible",  false);
-    navToggle.setAttribute("aria-expanded", false);
-    body.style.overflowY = "scroll";
-});
-
-/***************************************************************************/
-const tabs        = document.querySelector('.tabs');
-const btns        = document.querySelectorAll('.btn-projects--p');
-const allProjects = document.querySelectorAll('.project');
-
-function showTab(tab, element) {
-    btns.forEach(item => {
-        item.classList.remove("btn-projects--active");
-    })
-    allProjects.forEach(element => {
-        if (element.classList.contains(tab)) {
-            element.style.display = "block"
-        } else {
-            element.style.display = "none"
-        }
-    });
-    element.target.classList.add("btn-projects--active");
-}
-tabs.addEventListener('click', (element) => {
-    if(element.target.innerHTML === "All")           showTab("project",      element);
-    if(element.target.innerHTML === "Games")         showTab("game",         element);
-    if(element.target.innerHTML === "Websites")      showTab("website",      element);
-    if(element.target.innerHTML === "Web Apps")      showTab("app",          element);
-    if(element.target.innerHTML === "Landing Pages") showTab("landing-page", element);
-})
-
-/***************************************************************************/
-let mybutton = document.querySelector(".btn-top");
-
-window.onscroll = function() {
-    if (document.body.scrollTop > 1250 || document.documentElement.scrollTop > 1250) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-};
