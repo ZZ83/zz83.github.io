870dc36549b0fdf0e29612ea0b29e7ffcbe2e571
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-07-08 18:45:14 -0700

    Add slide animation to mobile navigation

--- script.js
+++ script.js
@@ -7,11 +7,30 @@ navToggle.addEventListener('click', () => {
     if (vis === "false") {
         mainNav  .setAttribute("data-visible",  true);
         navToggle.setAttribute("aria-expanded", true);
-        body.style.overflowY = "hidden";
+        // body.style.overflowY = "hidden";
 
     } else if (vis === "true") {
         mainNav  .setAttribute("data-visible",  false);
         navToggle.setAttribute("aria-expanded", false);
-        body.style.overflowY = "scroll"
+        // body.style.overflowY = "scroll"
     }
 })
+
+
+
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentState = button.getAttribute("data-state");
+
+    if (!currentState || currentState === "closed") {
+      button.setAttribute("data-state", "opened");
+      button.setAttribute("aria-expanded", "true");
+    } else {
+      button.setAttribute("data-state", "closed");
+      button.setAttribute("aria-expanded", "false");
+    }
+  });
+});
