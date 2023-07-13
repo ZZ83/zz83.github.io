ab0fd83928e537c387a4ea568dff2f0075f9ba9f
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-07-12 17:35:26 -0700

    Fix navigation not closing issue

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const body      = document.querySelector('body');
 const mainNav   = document.querySelector('.c-navigation');
 const navToggle = document.querySelector('.f-hamburgerMenu');
+const navItems  = document.querySelectorAll('.c-navigation__link');
 
 navToggle.addEventListener('click', () => {
     const vis = mainNav.getAttribute("data-visible");
@@ -15,3 +16,13 @@ navToggle.addEventListener('click', () => {
         // body.style.overflowY = "scroll"
     }
 })
+
+navItems.forEach(element => {
+    element.addEventListener('click', () => {
+        const vis = mainNav.getAttribute("data-visible");
+        if (vis === "true") {
+            mainNav  .setAttribute("data-visible",  false);
+            navToggle.setAttribute("aria-expanded", false);
+        }
+    })
+});
